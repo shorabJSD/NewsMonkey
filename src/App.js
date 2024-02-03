a5f774634d5dbf9bc06661b144f41f8cbb2519b8
shorabakka <shorabkka6004@gmail.com>
2024-02-04 03:04:04 +0600

    nothing changes of the code just saved thos of files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <NavBar/>
+      </div>
+    )
+  }
+}
+
