b53384e0caf968ff9b2081d9a1af0b31d632a6bb
shorabakka <shorabkka6004@gmail.com>
2024-02-05 01:14:58 +0600

    hellow world

--- src/components/News.js
+++ src/components/News.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
-export default class News extends Component {
+export default class News extends Component{
+
+  constructor(){
+    super();
+    console.log("this contructor function");
+  }
+
+
   render() {
     return (
       <div className='container my-3'>
-        <h1 className='my-3'>NewsMonkey - Top Headline</h1>
+        <h1 className='my-3'>NewsMonkey - Top </h1>
         <div className="row">
-             <div className="col-md-3">
-              <NewsItem title="Headline" description="about cricket world"/>
+             <div className="col-md-4">
+              <NewsItem myTitle="Headline" description="my description" imageUrl="https://images.prothomalo.com/prothomalo-bangla%2F2024-02%2F75f52690-83bc-411d-88f5-9b8c8ce8bf87%2FSean_Abbott_Cricket_Australia__1_.jpg?rect=0%2C0%2C683%2C384&auto=format%2Ccompress&fmt=webp&format=webp&w=640&dpr=1.0"/>
              </div>
-             <div className="col-md-3">
-              <NewsItem title="Headline" description="about cricket world"/>
+             <div className="col-md-4">
+              <NewsItem myTitle="Headline" description="my description"/>
              </div>
-             <div className="col-md-3">
-              <NewsItem title="Headline" description="about cricket world"/>
+             <div className="col-md-4">
+              <NewsItem myTitle="Headline" description="my description"/>
              </div>
         </div>
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-   let {title, description} = this.props;
+   let {myTitle, description, imageUrl} = this.props;
     return (
       <div>
          <div className="card" style={{width: '18rem'}}>
-          <img src="..." className="card-img-top" alt="..."/>
+          <img src={imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
-            <h5 className="card-title">{this.props.title}</h5>
-            <p className="card-text">{this.props.description}</p>
-            <a href="/newsdetails" className="btn btn-primary">Go somewhere</a>
+            <h5 className="card-title">{myTitle}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsdetails" className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>
       </div>
